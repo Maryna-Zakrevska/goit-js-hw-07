8667d53ee9cf7a9ce1cb1c65e320c4896b7c3475
Maryna-Zakrevska <lovemama.com.ua@gmail.com>
2021-10-21 02:32:57 +0300

    HW7

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,44 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery');
+
+const galleryMarkup = createGallery(galleryItems);
+
+galleryContainer.insertAdjacentHTML('afterbegin', galleryMarkup);
+
+
+function createGallery (items) {
+
+   return galleryItems.map(({preview, original, description}) => {
+       return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+   }).join('');
+
+};
+
+
+galleryContainer.addEventListener('click', onGalleryContainerClick);
+
+function onGalleryContainerClick(event) {event.preventDefault(); 
+  if (!event.target.classList.contains('gallery__image')) {return;} 
+  const imageUrl = event.target.dataset.source; 
+  const modal = basicLightbox.create(`<img src="${imageUrl}" alt="${event.target.alt}">`);
+  modal.show(); 
+  document.addEventListener('keydown', onEscapeCloseModal); 
+  function onEscapeCloseModal(event) {if (event.code === "Escape") {modal.close()}}};
+
+
+
+
+
+
